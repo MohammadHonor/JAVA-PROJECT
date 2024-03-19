7cf73f3caf3b56d3866579d54edd4470044057b3
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-03-19 12:32:45 +0530

    Employee

---
+++ com/Employ.java
@@ -0,0 +1,26 @@
+package com;
+public  class Employ{
+        private String name;
+        private int exp;
+        private double salary;
+       
+        public String getName() {
+            return name;
+        }
+        public double getSalary() {
+            return salary;
+        }
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getExp() {
+            return exp;
+        }
+        public void setExp(int exp) {
+            this.exp = exp;
+        }
+        
+}
